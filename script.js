6a4c80482f68540b936bb1e1867e671b3f6c9ab9
João Vitor <joaovitor55670@gmail.com>
2022-06-10 22:51:07 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let vid = document.getElementById("audio")
+let e = document.getElementById("a")
+
+// let teste = audioTracks.getTrackById("audio")
+
+function a() {
+    vid.play()
+    
+}
+
+console.log(vid.audioTracks)
+
+
