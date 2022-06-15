9b17ea1eac2726b43520d73f35196f9539ff1cb4
João Vitor <joaovitor55670@gmail.com>
2022-06-15 18:29:35 -0300

    sexto commit

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-var a = document.getElementsByClassName("imgg")[1]
-var audio = document.getElementById("audio")
+const audio = document.getElementById("audio")
+const $b = document.querySelector("#switch")
+const $c = document.querySelector("#switch2")
 
-function go(){
-    audio.play()
+audio.onloadeddata = function(){
+    let data = new Date(null);
+    data.setSeconds(audio.duration);
+    let duracao = data.toISOString().substr(12, 8);
+    console.log("O som tem duração " + duracao);
 }
 
-function next(){
+
+
+$b.addEventListener("click", function(){
+    $c.style = "display: inline-block"
+    $b.style = "display: none"
+    audio.play()}
+)
+$c.addEventListener("click", function(){
+    $c.style = "display: none"
+    $b.style = "display: inline-block"
+    audio.pause()}
+)
+
+
+function next() {
     audio.currentTime += 15
 }
 
-function back(){
-    audio.currentTime -=15
+function back() {
+    audio.currentTime -= 15
 }
